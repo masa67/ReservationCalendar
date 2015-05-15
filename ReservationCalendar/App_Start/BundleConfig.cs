3acbf1c2133a36d32da57429e88102b1f4c8b522
Matti Sangi <matti.sangi@deveteam.com>
2015-05-15 16:20:48 +0300

    Metronic initialization

--- ReservationCalendar/App_Start/BundleConfig.cs
+++ ReservationCalendar/App_Start/BundleConfig.cs
@@ -1,8 +1,27 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Optimization;
 
 namespace ReservationCalendar
 {
+    internal class AsIsBundleOrderer : IBundleOrderer
+    {
+        public virtual IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+
+    internal static class BundleExtensions
+    {
+        public static Bundle ForceOrdered(this Bundle sb)
+        {
+            sb.Orderer = new AsIsBundleOrderer();
+            return sb;
+        }
+    }
+
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
@@ -24,7 +43,6 @@ namespace ReservationCalendar
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
@@ -32,6 +50,33 @@ namespace ReservationCalendar
                       "~/Scripts/angular-route.js",
                       "~/Scripts/angular-resource.js"));
 
+            bundles.Add(new StyleBundle("~/bundles/metronic/css").ForceOrdered()
+                // BEGIN GLOBAL MANDATORY STYLES
+                .Include("~/Metronic/global/plugins/font-awesome/css/font-awesome.css")
+                .Include("~/Metronic/global/plugins/simple-line-icons/simple-line-icons.css")
+                .Include("~/Metronic/global/plugins/bootstrap/css/bootstrap.css")
+                .Include("~/Metronic/global/plugins/uniform/css/uniform.default.css")
+                .Include("~/Metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.css")
+                // END GLOBAL MANDATORY STYLES
+                // BEGIN PAGE LEVEL PLUGIN STYLES
+                .Include("~/Metronic/global/plugins/gritter/css/jquery.gritter.css")
+                .Include("~/Metronic/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css")
+                .Include("~/Metronic/global/plugins/fullcalendar/fullcalendar/fullcalendar.css")
+                .Include("~/Metronic/global/plugins/jqvmap/jqvmap/jqvmap.css")
+                // END PAGE LEVEL PLUGIN STYLES
+                // BEGIN PAGE STYLES
+                .Include("~/Metronic/admin/pages/css/tasks.css")
+                // END PAGE STYLES
+                // BEGIN THEME STYLES
+                .Include("~/Metronic/global/css/components.css")
+                .Include("~/Metronic/global/css/plugins.css")
+                .Include("~/Metronic/admin/layout/css/layout.css")
+                .Include("~/Metronic/admin/layout/css/themes/default.css")
+                .Include("~/Metronic/admin/layout/css/custom.css"));
+                // END THEME STYLES
+
+            bundles.Add(new ScriptBundle("~/bundles/metronic").Include());
+                      
             bundles.Add(new ScriptBundle("~/bundles/calendarApp").Include(
                       "~/Areas/Calendar/Scripts/client/app.js",
                       "~/Areas/Calendar/Scripts/client/Controllers/calendarCtrl.js"));
