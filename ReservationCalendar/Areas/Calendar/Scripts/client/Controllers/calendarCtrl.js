a7eb3c8e74269915706fa410e5474ea8f4b0a8c6
Matti Sangi <matti.sangi@pp.inet.fi>
2015-05-16 16:39:45 +0300

    Fix the calendar view to match width

--- ReservationCalendar/Areas/Calendar/Scripts/client/Controllers/calendarCtrl.js
+++ ReservationCalendar/Areas/Calendar/Scripts/client/Controllers/calendarCtrl.js
@@ -33,7 +33,7 @@ app.controller('CalendarCtrl', ['$scope', function ($scope) {
 
             angular.element($window).bind('resize', function () {
                 scope.$apply();
-            });           
+            });
 
             function modOnResize(width) {
                 if (Metronic.isRTL()) {
@@ -69,57 +69,57 @@ app.controller('CalendarCtrl', ['$scope', function ($scope) {
                         };
                     }
                 }
+
+                calBody.fullCalendar('destroy'); // destroy the calendar
+                calBody.fullCalendar({ //re-initialize the calendar
+                    header: h,
+                    defaultView: 'month', // change default view with available options from http://arshaw.com/fullcalendar/docs/views/Available_Views/ 
+                    slotMinutes: 15,
+                    editable: true,
+                    events: [{
+                        title: 'All Day Event',
+                        start: new Date(y, m, 1),
+                        backgroundColor: Metronic.getBrandColor('yellow')
+                    }, {
+                        title: 'Long Event',
+                        start: new Date(y, m, d - 5),
+                        end: new Date(y, m, d - 2),
+                        backgroundColor: Metronic.getBrandColor('green')
+                    }, {
+                        title: 'Repeating Event',
+                        start: new Date(y, m, d - 3, 16, 0),
+                        allDay: false,
+                        backgroundColor: Metronic.getBrandColor('red')
+                    }, {
+                        title: 'Repeating Event',
+                        start: new Date(y, m, d + 4, 16, 0),
+                        allDay: false,
+                        backgroundColor: Metronic.getBrandColor('green')
+                    }, {
+                        title: 'Meeting',
+                        start: new Date(y, m, d, 10, 30),
+                        allDay: false,
+                    }, {
+                        title: 'Lunch',
+                        start: new Date(y, m, d, 12, 0),
+                        end: new Date(y, m, d, 14, 0),
+                        backgroundColor: Metronic.getBrandColor('grey'),
+                        allDay: false,
+                    }, {
+                        title: 'Birthday Party',
+                        start: new Date(y, m, d + 1, 19, 0),
+                        end: new Date(y, m, d + 1, 22, 30),
+                        backgroundColor: Metronic.getBrandColor('purple'),
+                        allDay: false,
+                    }, {
+                        title: 'Click for Google',
+                        start: new Date(y, m, 28),
+                        end: new Date(y, m, 29),
+                        backgroundColor: Metronic.getBrandColor('yellow'),
+                        url: 'http://google.com/',
+                    }]
+                });
             }
-            
-            calBody.fullCalendar('destroy'); // destroy the calendar
-            calBody.fullCalendar({ //re-initialize the calendar
-                header: h,
-                defaultView: 'month', // change default view with available options from http://arshaw.com/fullcalendar/docs/views/Available_Views/ 
-                slotMinutes: 15,
-                editable: true,
-                events: [{
-                    title: 'All Day Event',
-                    start: new Date(y, m, 1),
-                    backgroundColor: Metronic.getBrandColor('yellow')
-                }, {
-                    title: 'Long Event',
-                    start: new Date(y, m, d - 5),
-                    end: new Date(y, m, d - 2),
-                    backgroundColor: Metronic.getBrandColor('green')
-                }, {
-                    title: 'Repeating Event',
-                    start: new Date(y, m, d - 3, 16, 0),
-                    allDay: false,
-                    backgroundColor: Metronic.getBrandColor('red')
-                }, {
-                    title: 'Repeating Event',
-                    start: new Date(y, m, d + 4, 16, 0),
-                    allDay: false,
-                    backgroundColor: Metronic.getBrandColor('green')
-                }, {
-                    title: 'Meeting',
-                    start: new Date(y, m, d, 10, 30),
-                    allDay: false,
-                }, {
-                    title: 'Lunch',
-                    start: new Date(y, m, d, 12, 0),
-                    end: new Date(y, m, d, 14, 0),
-                    backgroundColor: Metronic.getBrandColor('grey'),
-                    allDay: false,
-                }, {
-                    title: 'Birthday Party',
-                    start: new Date(y, m, d + 1, 19, 0),
-                    end: new Date(y, m, d + 1, 22, 30),
-                    backgroundColor: Metronic.getBrandColor('purple'),
-                    allDay: false,
-                }, {
-                    title: 'Click for Google',
-                    start: new Date(y, m, 28),
-                    end: new Date(y, m, 29),
-                    backgroundColor: Metronic.getBrandColor('yellow'),
-                    url: 'http://google.com/',
-                }]
-            });
         }
     };
 });
